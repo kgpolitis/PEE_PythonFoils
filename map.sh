4b2e9d0e63f5024b9a372fc1b03055c16af4fdfc
Kostas G Pol <kgpolitis@gmail.com>
2021-06-22 12:57:41 +0200

    Bash tools to use different versions
    Since we have different version, what if we want to compare the
    behaviour of each version ? Check out these very simple bash tools

---
+++ map.sh
@@ -0,0 +1,7 @@
+echo 'Local Mapping to FCFoil : ForPython3 : version '$1
+ln -sf ../../PythonFOILS/V$1/ForPython3/FCFoil.py
+ln -sf ../../PythonFOILS/V$1/ForPython3/FOIL.py
+ln -sf ../../PythonFOILS/V$1/ForPython3/NACA_LIB.py
+ln -sf ../../PythonFOILS/V$1/ForPython3/WAGENINGEN_LIB.py
+
+
---
+++ map2lib/map27.sh
@@ -0,0 +1,5 @@
+echo 'Local Mapping to FCFoil : ForPython27 : version '$1
+ln -sf ../../PythonFOILS/V$1/ForPython27/FCFoil.py
+ln -sf ../../PythonFOILS/V$1/ForPython27/FOIL.py
+ln -sf ../../PythonFOILS/V$1/ForPython27/NACA_LIB.py
+ln -sf ../../PythonFOILS/V$1/ForPython27/WAGENINGEN_LIB.py
